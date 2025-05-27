7aabafe51d62542d4ece16808552b76485ab84a5
Hadi Basabr <basabr_hadi@yahoo.com>
2025-05-27 11:58:48 +0330

    Update MountainHut.java

--- lab04/src/MountainHut.java
+++ lab04/src/MountainHut.java
@@ -12,57 +12,48 @@ public class MountainHut {
 
     private final String name;
     private final Optional<Integer> altitude;
-    private final String category;
-    private final Integer bedsNumber;
-    private final Municipality municipality;
+    private String category;
+    private Integer bedsNumber;
+    private Municipality municipality;
 
     public MountainHut(String name, Optional<Integer> altitude, String category,
                        Integer bedsNumber, Municipality municipality) {
         this.name = name;
-        this.altitude = altitude;
+        this.altitude = altitude != null ? altitude : Optional.empty();
         this.category = category;
         this.bedsNumber = bedsNumber;
         this.municipality = municipality;
     }
 
-    /**
-     * Retrieves the name of the hut
-     * @return name of the hut
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * Retrieves altitude if available
-     * 
-     * @return optional hut altitude
-     */
     public Optional<Integer> getAltitude() {
         return altitude;
     }
 
-    /**
-     * Retrieves the category of the hut
-     * @return hut category
-     */
     public String getCategory() {
         return category;
     }
 
-    /**
-     * Retrieves the number of beds available in the hut
-     * @return number of beds
-     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public Integer getBedsNumber() {
         return bedsNumber;
     }
 
-    /**
-     * Retrieves the municipality of the hut
-     * @return hut municipality
-     */
+    public void setBedsNumber(Integer bedsNumber) {
+        this.bedsNumber = bedsNumber;
+    }
+
     public Municipality getMunicipality() {
         return municipality;
+    }
+
+    public void setMunicipality(Municipality municipality) {
+        this.municipality = municipality;
     }
 }
